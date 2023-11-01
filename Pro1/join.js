804a4125b38e24d2ef8aa0e0a8ead0cf7040ddac
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-11-01 18:32:11 +0900

    .

---
+++ Pro1/join.js
@@ -0,0 +1,18 @@
+// let inputId = document.getElementById("id");
+// let inputpasw = document.querySelector(".pasw");
+// let inputChpasw = document.querySelector(".chk_pasw");
+
+// // 경고 태그들
+// let Wid = document.getElementById("Wid");
+// let Wpasw = document.querySelector(".Wpasw");
+// let ChWpasw = document.querySelector(".ChWpasw");
+
+// let subBtn = document.querySelector(".sub_btn");
+
+// subBtn.onclick = (event) => {
+//     event.preventDefault();
+//     console.log(inputId.value.length);
+//     if(inputId.value.length === 0){
+//         Wid.style.display = 'none';
+//     }
+// };
