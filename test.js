da67accc2ddb746b21c603d061119470db739fb3
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-12-15 16:11:27 +0900

    프젝 2 시작

---
+++ test.js
@@ -0,0 +1,12 @@
+
+
+let Qucount = document.querySelector(".QCount").textContent;
+console.log(Qucount);
+console.log(this.Qucount);
+
+// function test(){
+//     var Qucount2 = document.querySelector(".QCount").textContent;
+//     console.log(this.Qucount2);
+// }
+
+// test();
