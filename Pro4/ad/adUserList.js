fd465a9e619ca708057f076f1ca75a61463d3746
jeongbe <bini8042@naver.com>
2025-08-28 16:14:58 +0900

    괸리자 유저리스트, 정지팝업, 유저부가정보 팝업 작업

---
+++ Pro4/ad/adUserList.js
@@ -0,0 +1,15 @@
+function UserStopPopup() {
+    window.open(
+        "adUserStopPop.html",       // 팝업에 띄울 페이지
+        "UserStopPop",      // 팝업창 이름 (중복 클릭 시 같은 창 재사용)
+        "width=270,height=280,top=100,left=200,scrollbars=no,resizable=yes"
+    );
+}
+
+function UserInfo() {
+    window.open(
+        "adUserInfoPop.html",       // 팝업에 띄울 페이지
+        "UserInfo",      // 팝업창 이름 (중복 클릭 시 같은 창 재사용)
+        "width=850,height=390,top=100,left=200,scrollbars=no,resizable=yes"
+    );
+}
